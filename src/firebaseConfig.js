17e2a44cdbcde3118f0b1ac5671fe77bb9a37bf5
HydroOx1d <n-esenov44@mail.ru>
2022-03-30 12:53:38 +0600

    added .env variables

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -3,13 +3,13 @@ import {getAuth} from 'firebase/auth'
 import {getDatabase} from 'firebase/database'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyACpBoj1vF8t_rJvMvuo4InyTMQ4KsV6cU",
-  authDomain: "todo-in-react-21a68.firebaseapp.com",
-  databaseURL: 'https://todo-in-react-21a68-default-rtdb.firebaseio.com/',
-  projectId: "todo-in-react-21a68",
-  storageBucket: "todo-in-react-21a68.appspot.com",
-  messagingSenderId: "695457836653",
-  appId: "1:695457836653:web:fb6ce4ee669fd2d0501964",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDERID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 

1ef26a1324584a43c05f8c54ea847296c440c2c6
HydroOx1d <n-esenov44@mail.ru>
2022-04-07 21:12:25 +0600

    Fixed shit

--- src/components/todo-add/TodoAddContainer.jsx
+++ src/components/todo-add/TodoAddContainer.jsx
@@ -9,7 +9,7 @@ const TodoAddContainer = (props) => {
 
   const onAddTask = (formObj) => {
     let result = "";
-    let num = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9].map((el) => el.toString());
+    let num = '0123456789'.split('');
     let str = "abcdefghijklmnopqrstuvwxyz".split('');
 
     for (let i = 0; i <= 10; i++) {

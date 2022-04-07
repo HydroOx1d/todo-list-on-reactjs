6b64f833a870a19cd32517e4dd055a66f0d6d949
HydroOx1d <n-esenov44@mail.ru>
2022-04-07 21:16:06 +0600

    change text

--- src/components/todo-tasks/TodoTasks.jsx
+++ src/components/todo-tasks/TodoTasks.jsx
@@ -7,17 +7,27 @@ const TodoTasks = (props) => {
   return (
     <>
       {props.tasks.length === 0 ? (
-        <h2 className="main-padding">Задачи отсутствуют</h2>
+        <h2 className="main-padding">There are no tasks</h2>
       ) : (
         <div className="todo-tasks main-padding">
           {props.tasks.map((task) => {
             return (
               <div className="todo-task" key={task.data.id}>
                 <span className="todo-task__number">----------</span>
-                <h4 className="todo-task__title"> <NavLink to={"/task/" + task.taskId}>{task.data.title}</NavLink> </h4>
-                <span onClick={() => {
-                  remove(ref(database, props.uid + "/tasks/" + task.taskId));
-                }} className="todo-task__delete">Delete</span>
+                <h4 className="todo-task__title">
+                  {" "}
+                  <NavLink to={"/task/" + task.taskId}>
+                    {task.data.title}
+                  </NavLink>{" "}
+                </h4>
+                <span
+                  onClick={() => {
+                    remove(ref(database, props.uid + "/tasks/" + task.taskId));
+                  }}
+                  className="todo-task__delete"
+                >
+                  Delete
+                </span>
               </div>
             );
           })}

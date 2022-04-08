1e2d5a245be1dfce0df694fa86f47c92d7624546
HydroOx1d <n-esenov44@mail.ru>
2022-04-08 17:17:25 +0600

    Deploying a project on GitHub

--- src/index.js
+++ src/index.js
@@ -4,15 +4,15 @@ import './index.css';
 import App from './App';
 import { Provider } from 'react-redux';
 import store from './redux/store'
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <HashRouter>
       <Provider store={store}>
         <App />
       </Provider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
   document.getElementById("root")
 );

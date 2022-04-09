47f0996a54dc11d7b7096369b3eb8b9f0b9b9672
HydroOx1d <n-esenov44@mail.ru>
2022-04-09 17:35:01 +0600

    Added dynamic imports for code separation

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import React from "react";
+import React, { Suspense } from "react";
 import "./App.css";
 import { HashRouter, Route, Routes } from "react-router-dom";
-import TodoDetailContainer from "./components/todo-detail/TodoDetailContainer";
-import TodoMainContainer from "./components/todo-main/TodoMainContainer";
-import LoginContainer from "./components/login/LoginContainer";
-import RegisterContainer from "./components/register/RegisterContainer";
 import { Provider } from "react-redux";
 import store from "./redux/store";
+import { MainPreloader } from "./components/common/loader/MainPreloader";
+
+const TodoDetailContainer = React.lazy(() => import('./components/todo-detail/TodoDetailContainer'))
+const TodoMainContainer = React.lazy(() => import('./components/todo-main/TodoMainContainer'))
+const LoginContainer = React.lazy(() => import('./components/login/LoginContainer'))
+const RegisterContainer = React.lazy(() => import('./components/register/RegisterContainer'))
 
 const App = (props) => {
   return (
     <HashRouter>
       <Provider store={store}>
         <div className="todo">
-          <Routes>
-            <Route path="/" element={<TodoMainContainer />} />
-            <Route path="/login" element={<LoginContainer />} />
-            <Route path="/register" element={<RegisterContainer />} />
-            <Route path="/task/:id" element={<TodoDetailContainer />} />
-          </Routes>
+          <Suspense fallback={<MainPreloader/>}>
+            <Routes>
+              <Route path="/" element={<TodoMainContainer />} />
+              <Route path="/login" element={<LoginContainer />} />
+              <Route path="/register" element={<RegisterContainer />} />
+              <Route path="/task/:id" element={<TodoDetailContainer />} />
+            </Routes>
+          </Suspense>
         </div>
       </Provider>
     </HashRouter>
